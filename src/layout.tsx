3ff482221e171bba08d89380eac0b58f44b5bdd2
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-10-07 00:26:06 +0700

    Add style

--- src/layout.tsx
+++ src/layout.tsx
@@ -6,7 +6,8 @@ export default function RootLayout() {
       <head>
         <meta charSet='UTF-8' />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Lmao Tus</title>
+        <title>Lmao Tus?</title>
+        <link rel="stylesheet" href="style.css" />
       </head>
       <body>
         <div id="root">
