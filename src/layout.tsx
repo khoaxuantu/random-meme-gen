9943087e4e3b6d4cbf261a421dc3033e0770e89d
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-10-07 15:17:39 +0700

    Add icon & meta

--- src/layout.tsx
+++ src/layout.tsx
@@ -6,8 +6,19 @@ export default function RootLayout() {
       <head>
         <meta charSet='UTF-8' />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="apple-touch-icon" href="public/logo/apple-touch-icon.png" />
+        <link rel="icon" href="public/logo/favicon-32x32.png" type="image/png" />
+        <link rel="icon" href="public/logo/favicon-64x64.png" type="image/png" />
+        <link rel="manifest" href="public/logo/site.webmanifest" />
+
         <title>Lmao Tus?</title>
-        <link rel="stylesheet" href="style.css" />
+        <meta name="title" content='Lmao Tuslipid???' />
+        <meta name="description" content="A random meme picker created by Xuan Khoa Tu Nguyen.
+          Pick a meme a day for happier life ~" />
+        <meta name='author' content='Xuan Khoa Tu Nguyen' />
+
+        <link rel="stylesheet" href="public/style.css" />
       </head>
       <body>
         <div id="root">
