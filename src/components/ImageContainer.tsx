8fe6129fb53876b85cd5ee2ccc10fd6a6dc86b96
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-10-29 00:18:13 +0700

    Optimize main page

--- src/components/ImageContainer.tsx
+++ src/components/ImageContainer.tsx
@@ -4,9 +4,9 @@ export default function ImageContainer() {
 
   return(
     <div className="sl-l-container main-l-container m-5">
-      <h3 className="mb-5">Welcome to my randome meme picker 😃</h3>
       <div className="row main-l-container__body">
-        <div className="col-4 pb-3 ps-2 pe-2 main-c-blockquote">
+        <div className="col-4 pb-4 main-c-blockquote">
+        <h3 className="mb-5">Welcome to my random meme picker 😃</h3>
           <blockquote className="mb-4">
             All of the memes in my collection are stolen from the internet
           </blockquote>
@@ -16,7 +16,7 @@ export default function ImageContainer() {
             </button>
           </div>
         </div>
-        <div className="col-8 ps-2 pe-2 main-c-image__box">
+        <div className="col-8 main-c-image__box">
           <img src="save_in_prod.png" alt="" />
         </div>
       </div>
