a7797f553b4c05a30df2460b20c97f9529cd2e03
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-10-28 21:52:25 +0700

    Update layout

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,9 +1,17 @@
-import App from './pages/App';
+import Navbar from './components/Navbar';
 import { Metadata } from './utils/Metadata';
 
 interface RootLayoutProps {
-  metadata: Metadata,
+  metadata?: Metadata,
   children: React.ReactNode
+}
+
+const DEFAULT_METADATA: Metadata = {
+  title: "Lmao Tus?",
+  metaTitle: "Lmao Tuslipid???",
+  description: `A random meme picker created by Xuan Khoa Tu Nguyen.
+  Pick a meme a day for a happier life ~`,
+  author: "Xuan Khoa Tu Nguyen"
 }
 
 export default function RootLayout(props: RootLayoutProps) {
@@ -18,17 +26,16 @@ export default function RootLayout(props: RootLayoutProps) {
         <link rel="icon" href="logo/favicon-32x32.png" type="image/png" />
         <link rel="manifest" href="logo/site.webmanifest" />
 
-        <title>{ props.metadata.title || "Page" }</title>
-        <meta name="title" content={props.metadata.metaTitle} />
-        <meta name="description" content={props.metadata.description} />
-        <meta name='author' content={props.metadata.author} />
+        <title>{ props.metadata?.title ?? DEFAULT_METADATA.title }</title>
+        <meta name="title" content={ props.metadata?.metaTitle ?? DEFAULT_METADATA.metaTitle } />
+        <meta name="description" content={ props.metadata?.description ?? DEFAULT_METADATA.description } />
+        <meta name='author' content={ props.metadata?.author ?? DEFAULT_METADATA.author } />
 
         <link rel="stylesheet" href="style.css" />
       </head>
       <body>
-        <div id="root">
-          { props.children }
-        </div>
+        <Navbar />
+        { props.children }
       </body>
     </html>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import ImageContainer from "@components/ImageContainer";
 import RootLayout from "@/layout";
-import { Metadata } from "@/utils/Metadata";
-import Navbar from "@/components/Navbar";
-
-const metadata: Metadata = {
-  title: "Lmao Tus?",
-  metaTitle: "Lmao Tuslipid???",
-  description: `A random meme picker created by Xuan Khoa Tu Nguyen.
-  Pick a meme a day for a happier life ~`,
-  author: "Xuan Khoa Tu Nguyen"
-}
 
 export default function App() {
   return (
     <>
       <React.StrictMode>
-        <RootLayout metadata={metadata}>
-          <Navbar />
+        <RootLayout>
           <ImageContainer />
         </RootLayout>
       </React.StrictMode>
